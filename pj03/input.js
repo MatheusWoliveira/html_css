8907010b5fd81b0316afef6d847a8927760edd74
Matheus Oliveira <137079454+MatheusWoliveira@users.noreply.github.com>
2023-12-09 11:27:01 -0300

    atualizado

--- pj03/input.js
+++ pj03/input.js
@@ -8,3 +8,20 @@ function menuShow(){
         document.querySelector('.icon').src = "midia/close_white_36dp.svg"
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ScrollReveal().reveal('.container', { delay: 200 });
+    ScrollReveal().reveal('.maps', { delay: 200 });
+});
